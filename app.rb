fcfc1dc7659f64a24cd7bb8756359cb58d525aa8
fabiogm <fmagalhaes@gmail.com>
2014-04-15 00:16:31 -0300

    Fixing app.rb. Ready to receive more code :)

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
+require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/config_file'
-require 'sinatra/contrib'
+#require 'sinatra/contrib'
 require 'sinatra/activerecord'
 require 'erb'
 require 'yaml'
@@ -10,19 +11,21 @@ require './lib/helpers'
 class State < ActiveRecord::Base
 end
 
-class App < Sinatra::Base
-  register Sinatra::ConfigFile
-  register Sinatra::ActiveRecordExtension
+register Sinatra::ConfigFile
+register Sinatra::ActiveRecordExtension
 
-  get '/' do
-    "NEX DCP30 API"
-  end
+get '/' do
+  "NEX DCP30 API"
+end
 
-  configure do
-    #config_file 'config/database.yml'
-  end
+get '/api/state/:name' do
+  "Future"
+end
 
-  configure :development do
-    set :reload_stuff, true
-  end
+configure do
+  #config_file 'config/database.yml'
+end
+
+configure :development do
+  set :reload_stuff, true
 end
